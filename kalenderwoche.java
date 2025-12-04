5a7027267484f2e5f5d923bb200ecf8f00fcf87b
omid mohammadi <omid.2005mohammadi@gmail.com>
2025-12-04 10:45:07 +0100

    aenderung am kalenderwoche.java

--- kalenderwoche.java
+++ kalenderwoche.java
@@ -9,21 +9,23 @@ public class kalenderwoche{
 	public static void kalenderwoche_ausgeben(){
 	
 		Scanner sc = new Scanner(System.in);
+		int kalenderwoche;
 		while(true){
 			System.out.print("KW?");
 			int kw = sc.nextInt();
-			int P2 = 1;
-			int P3 = 2;
-			int P4 = 3;
-			int P5 = 4;
-			int P1 = 5;
 			if(kw > 52 || kw < 1){
 				System.out.println("Falsche KW");
 			}else{
-
+				kalenderwoche = kw;
+				break;
 			}
 		}
-		
+		int[] p1 = new int[]{5,10,15,20,25,30,35,40,45,50};
+		for(int i = 0; i < p1.length; i++){
+			if(p1[i] == kalenderwoche){
+				System.out.println("Person 1");
+			}
+		}
 		
 		
 	}
